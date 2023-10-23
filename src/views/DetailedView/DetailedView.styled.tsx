a0220aa966c6e19eddaf8a702fe6119c65e6f14b
pawelberowski <pawelberowski@gmail.com>
2023-10-23 14:16:04 +0200

    feat(DetailedView): improved styling

--- src/views/DetailedView/DetailedView.styled.tsx
+++ src/views/DetailedView/DetailedView.styled.tsx
@@ -3,30 +3,54 @@ import { Box, Button, styled } from "@mui/material";
 export const DetailedViewContainer = styled(Box)`
   display: flex;
   gap: 10%;
+  font-family: Poppins, sans-serif;
 `;
 
 export const LeftPanelContainer = styled(Box)`
   max-width: 60%;
 `;
 
-export const NameAndScoreContainer = styled(Box)``;
+export const NameAndScoreContainer = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+`;
 export const NameAndAddressContainer = styled(Box)``;
 
-export const NameWrapper = styled(Box)``;
+export const NameWrapper = styled(Box)`
+  font-size: 40px;
+  font-weight: 600;
+  letter-spacing: 1.2px;
+`;
 
-export const AddressWrapper = styled(Box)``;
+export const AddressWrapper = styled(Box)`
+  font-size: 25px;
+  font-weight: 400;
+  letter-spacing: 0.75px;
+`;
 
 export const ScoreContainer = styled(Box)``;
 
-export const ReviewsNumberWrapper = styled(Box)``;
+export const ReviewsNumberWrapper = styled(Box)`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: 0.42px;
+`;
 
 export const RatingWrapper = styled(Box)``;
 export const PhotoSliderContainer = styled(Box)``;
 
-export const DisplaySwitchesContainer = styled(Box)``;
+export const DisplaySwitchesContainer = styled(Box)`
+  display: flex;
+  justify-content: space-around;
+`;
 
 export const DisplaySwitchWrapper = styled(Button)`
   text-transform: lowercase;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: 0.48px;
 `;
 
 export const DisplayContainer = styled(Box)``;
