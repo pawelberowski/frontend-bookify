98fc52ea5e3a59f23c45f4d8425ebe63a9da51ce
pawelberowski <pawelberowski@gmail.com>
2023-10-23 12:00:48 +0200

    feat(DetailedView): added scaffolding for the view

---
+++ src/views/DetailedView/DetailedView.styled.tsx
@@ -0,0 +1,42 @@
+import { Box, Button, styled } from "@mui/material";
+
+export const DetailedViewContainer = styled(Box)`
+  display: flex;
+  gap: 10%;
+`;
+
+export const LeftPanelContainer = styled(Box)`
+  max-width: 60%;
+`;
+
+export const NameAndScoreContainer = styled(Box)``;
+export const NameAndAddressContainer = styled(Box)``;
+
+export const NameWrapper = styled(Box)``;
+
+export const AddressWrapper = styled(Box)``;
+
+export const ScoreContainer = styled(Box)``;
+
+export const ReviewsNumberWrapper = styled(Box)``;
+
+export const RatingWrapper = styled(Box)``;
+export const PhotoSliderContainer = styled(Box)``;
+
+export const DisplaySwitchesContainer = styled(Box)``;
+
+export const DisplaySwitchWrapper = styled(Button)`
+  text-transform: lowercase;
+`;
+
+export const DisplayContainer = styled(Box)``;
+
+export const RightPanelContainer = styled(Box)``;
+
+export const BookingContainer = styled(Box)``;
+
+export const ContactContainer = styled(Box)``;
+
+export const SocialMediaContainer = styled(Box)``;
+
+export const LabelWrapper = styled(Box)``;
--- src/views/DetailedView/index.tsx
+++ src/views/DetailedView/index.tsx
@@ -1,6 +1,25 @@
-import { Box } from "@mui/material";
 import { useVenueDetails } from "./useVenueDetails.tsx";
 import { useParams } from "react-router-dom";
+import {
+  AddressWrapper,
+  BookingContainer,
+  ContactContainer,
+  DetailedViewContainer,
+  DisplayContainer,
+  DisplaySwitchesContainer,
+  DisplaySwitchWrapper,
+  LabelWrapper,
+  LeftPanelContainer,
+  NameAndAddressContainer,
+  NameAndScoreContainer,
+  NameWrapper,
+  PhotoSliderContainer,
+  RatingWrapper,
+  ReviewsNumberWrapper,
+  RightPanelContainer,
+  ScoreContainer,
+  SocialMediaContainer,
+} from "./DetailedView.styled.tsx";
 
 export const DetailedView = () => {
   const params = useParams();
@@ -9,10 +28,42 @@ export const DetailedView = () => {
 
   return (
     venueDetails && (
-      <Box>
-        <h2>Detailed View of venueId: {venueDetails.venueId}</h2>
-        <p>Description: {venueDetails.description}</p>
-      </Box>
+      <DetailedViewContainer>
+        <LeftPanelContainer>
+          <NameAndScoreContainer>
+            <NameAndAddressContainer>
+              <NameWrapper>{venueDetails.name}</NameWrapper>
+              <AddressWrapper>{`${venueDetails.location.postalCode}, ${venueDetails.location.name}`}</AddressWrapper>
+            </NameAndAddressContainer>
+            <ScoreContainer>
+              <ReviewsNumberWrapper>
+                {venueDetails.numberOfReviews} reviews
+              </ReviewsNumberWrapper>
+              <RatingWrapper>{venueDetails.rating}</RatingWrapper>
+            </ScoreContainer>
+          </NameAndScoreContainer>
+          <PhotoSliderContainer>Placeholder</PhotoSliderContainer>
+          <DisplaySwitchesContainer>
+            <DisplaySwitchWrapper variant="text">
+              description
+            </DisplaySwitchWrapper>
+            <DisplaySwitchWrapper variant="text">gallery</DisplaySwitchWrapper>
+            <DisplaySwitchWrapper variant="text">map</DisplaySwitchWrapper>
+          </DisplaySwitchesContainer>
+          <DisplayContainer>{venueDetails.description}</DisplayContainer>
+        </LeftPanelContainer>
+        <RightPanelContainer>
+          <BookingContainer>
+            <LabelWrapper>Book this venue</LabelWrapper>
+          </BookingContainer>
+          <ContactContainer>
+            <LabelWrapper>Contact this venue</LabelWrapper>
+          </ContactContainer>
+          <SocialMediaContainer>
+            <LabelWrapper>Check out on social media</LabelWrapper>
+          </SocialMediaContainer>
+        </RightPanelContainer>
+      </DetailedViewContainer>
     )
   );
 };
