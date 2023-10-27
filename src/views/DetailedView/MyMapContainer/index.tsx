dc5c2b9a06e32e405260a8b9646c4f149d08e724
pawelberowski <pawelberowski@gmail.com>
2023-10-27 12:53:06 +0200

    feat(Map): installed react-leaflet and added map

---
+++ src/views/DetailedView/MyMapContainer/index.tsx
@@ -0,0 +1,18 @@
+import { Marker, Popup, TileLayer } from "react-leaflet";
+import { MapWrapper } from "./MyMapContainer.styled.tsx";
+
+export const MyMapContainer = () => {
+  return (
+    <MapWrapper center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapWrapper>
+  );
+};
