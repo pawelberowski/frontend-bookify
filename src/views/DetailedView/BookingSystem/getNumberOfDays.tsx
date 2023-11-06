8a348ee6e8b09379de707fdf32976296d695363b
pawelberowski <pawelberowski@gmail.com>
2023-11-06 14:38:25 +0100

    refactor(getNumberOfDays) moved constant above function

--- src/views/DetailedView/BookingSystem/getNumberOfDays.tsx
+++ src/views/DetailedView/BookingSystem/getNumberOfDays.tsx
@@ -1,13 +1,13 @@
 import { DatesValues } from "./useCalendarDates.tsx";
+const ONE_DAY = 24 * 60 * 60 * 1000;
 
 export const getNumberOfDays = (value: DatesValues) => {
   if (Array.isArray(value)) {
     const startDate = value[0]?.getTime();
     const endDate = value[1]?.getTime();
     if (startDate !== endDate) {
-      const oneDay = 24 * 60 * 60 * 1000;
       return Math.round(
-        Math.abs(((endDate ?? 2 * oneDay) - (startDate ?? oneDay)) / oneDay),
+        Math.abs(((endDate ?? 2 * ONE_DAY) - (startDate ?? ONE_DAY)) / ONE_DAY),
       );
     }
     return 1;
