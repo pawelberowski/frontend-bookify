755ef736939934f789d963a99bb9051ed3c8919f
pawelberowski <pawelberowski@gmail.com>
2023-11-07 17:28:19 +0100

    feat(Header) header now have its mobile version

--- src/shared/components/Layout/Header/index.tsx
+++ src/shared/components/Layout/Header/index.tsx
@@ -5,26 +5,43 @@ import {
   ShadowButton,
   StyledAppBar,
 } from "./Header.styled.tsx";
-import { Typography } from "@mui/material";
-
+import { Box, Typography } from "@mui/material";
+import { useIsMobileView } from "../../../utils/useIsMobileView.tsx";
+import HamburgerIcon from "../../../assets/hamburger.svg";
 export const Header = () => {
+  const isMobileView = useIsMobileView();
+
   return (
-    <StyledAppBar color="inherit">
-      <Logo>BOOKIFY</Logo>
-      <LinksContainer>
-        <ShadowButton href="/about" color="inherit">
-          <Typography variant="subtitle1">about us</Typography>
-        </ShadowButton>
-        <LowerCaseButton href="/favourites" color="inherit">
-          <Typography variant="subtitle1">your favorites</Typography>
-        </LowerCaseButton>
-        <LowerCaseButton href="/hosting" color="inherit">
-          <Typography variant="subtitle1">start hosting</Typography>
-        </LowerCaseButton>
-        <LowerCaseButton href="/login" color="primary">
-          <Typography variant="subtitle1">login</Typography>
-        </LowerCaseButton>
-      </LinksContainer>
-    </StyledAppBar>
+    <Box>
+      {isMobileView ? (
+        <StyledAppBar color="inherit">
+          <img src={HamburgerIcon} alt="Three horizontal lines" />
+          <Logo>BOOKIFY</Logo>
+          <LinksContainer>
+            <LowerCaseButton href="/login" color="primary">
+              <Typography variant="subtitle1">login</Typography>
+            </LowerCaseButton>
+          </LinksContainer>
+        </StyledAppBar>
+      ) : (
+        <StyledAppBar color="inherit">
+          <Logo>BOOKIFY</Logo>
+          <LinksContainer>
+            <ShadowButton href="/about" color="inherit">
+              <Typography variant="subtitle1">about us</Typography>
+            </ShadowButton>
+            <LowerCaseButton href="/favourites" color="inherit">
+              <Typography variant="subtitle1">your favorites</Typography>
+            </LowerCaseButton>
+            <LowerCaseButton href="/hosting" color="inherit">
+              <Typography variant="subtitle1">start hosting</Typography>
+            </LowerCaseButton>
+            <LowerCaseButton href="/login" color="primary">
+              <Typography variant="subtitle1">login</Typography>
+            </LowerCaseButton>
+          </LinksContainer>
+        </StyledAppBar>
+      )}
+    </Box>
   );
 };
