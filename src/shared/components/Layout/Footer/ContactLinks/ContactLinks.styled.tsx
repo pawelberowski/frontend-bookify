7cc0a6bf9d278224d38ba67bccefea99e4bcf62f
pawelberowski <pawelberowski@gmail.com>
2023-11-21 11:47:13 +0100

    feat(Footer) improved styling on mobile

--- src/shared/components/Layout/Footer/ContactLinks/ContactLinks.styled.tsx
+++ src/shared/components/Layout/Footer/ContactLinks/ContactLinks.styled.tsx
@@ -1,8 +1,13 @@
 import { Box, styled } from "@mui/material";
 
-export const ContactLinksStyled = styled(Box)`
+export const ContactLinksStyled = styled(Box)(
+  ({ theme }) => `
   display: flex;
   justify-content: space-around;
   margin: 52px 0 52px 0;
-  min-width: 600px;
-`;
+  width: 600px;
+  ${theme.breakpoints.down("md")} {
+    width: 100%;
+  }
+`,
+);
--- src/shared/components/Layout/Footer/PhraseContainer/index.tsx
+++ src/shared/components/Layout/Footer/PhraseContainer/index.tsx
@@ -4,10 +4,12 @@ import { PhraseContainerStyled } from "./PhraseContainer.styled.tsx";
 export const PhraseContainer = () => {
   return (
     <PhraseContainerStyled>
-      <Typography variant="body2">
+      <Typography variant="body2" textAlign="center">
         Feel free to contact us in case of any problems and doubts.
       </Typography>
-      <Typography variant="body2">We are here for you.</Typography>
+      <Typography variant="body2" textAlign="center">
+        We are here for you.
+      </Typography>
     </PhraseContainerStyled>
   );
 };
