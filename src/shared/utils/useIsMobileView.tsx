ea4f6f4072a80947a95c963bfa6fc65dc64d3838
pawelberowski <pawelberowski@gmail.com>
2024-01-13 14:47:57 +0100

    refactor(useIsMobileView): refactored code for better readability

--- src/shared/utils/useIsMobileView.tsx
+++ src/shared/utils/useIsMobileView.tsx
@@ -2,7 +2,7 @@ import { useMediaQuery, useTheme } from "@mui/material";
 
 export const useIsMobileView = () => {
   const bookifyTheme = useTheme();
-  return useMediaQuery((theme: typeof bookifyTheme) =>
-    theme.breakpoints.down("md"),
-  );
+  return useMediaQuery((theme: typeof bookifyTheme) => {
+    return theme.breakpoints.down("md");
+  });
 };
